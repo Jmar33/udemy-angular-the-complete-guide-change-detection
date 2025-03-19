2b8131d91898e9d544ab75b9be79c33658f0f23b
Jmar33 <josimarferreira922@yahoo.com.br>
2025-03-18 21:37:07 -0300

    feat: implementing onpush strategy

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,6 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
+
+// OnPush strategy is a powerful resource that allows the change detection just occurs when we have 
+// a change in a input, in a event inside a component, from a child component, an event in the component itself or 
+// when we have a manually change
 
 @Component({
   selector: 'app-counter',
@@ -8,9 +12,23 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  private zone = inject(NgZone);
   count = signal(0);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.count.set(0)
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('time expired!')
+      }, 5000);
+    });
+  }
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
